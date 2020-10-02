0c336ccaecac77f74514a2b7a67db2f088c3640b
dylan2050 <dyhayg@gmail.com>
2020-10-02 11:25:19 +0200

    Basic settings to start assignment

---
+++ app/src/main/java/com/example/madlevel4task2/ui/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.madlevel4task2.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+import com.example.madlevel4task2.R
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+        supportActionBar?.title = "MAD Level 4 Task 2"
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+}
